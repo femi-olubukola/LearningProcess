f226ad12a28ba911c8fc7da36158015cccc92d07
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-02-09 04:20:31 +0100

    initialCommit++

--- src/ClassAverage2.java
+++ src/ClassAverage2.java
@@ -1,3 +1,69 @@
+
+// Formulating Algorithms: Sentinel-Controlled
+//        Repetition
+
+// PROBLEM STATEMENT
+
+//  Develop a class-averaging program that processes grades for an arbitrary number of
+//        students each time it’s run.
+
+//  Developing the Pseudocode Algorithm with TOP-DOWN, STEPWISE REFINEMENT:
+//        The Top and First Refinement
+
+// TOP—a single statement that conveys the overall
+//        function of the program:
+
+//                                 Determine the class average for the quiz
+
+// FIRST REFINEMENT:
+
+//        Initialize variables
+//        Input, sum and count the quiz grades
+//        Calculate and print the class average
+
+// BREAKING DOWN REFINEMENT STATEMENTS (proceeding to the second refinement)
+
+// Initialize variables :::  -Initialize total to zero
+//                           -Initialize counter to zero
+
+// Input, sum and count the quiz grades :::  -Prompt the user to enter the first grade
+//                                           -Input the first grade (possibly the sentinel)
+//                                           -While the user has not yet entered the sentinel
+//                                              -Add this grade into the running total
+//                                              -Add one to the grade counter
+//                                              -Prompt the user to enter the next grade
+//                                              -Input the next grade (possibly the sentinel)
+
+// Calculate and print the class average ::: -If the counter is not equal to zero
+//                                              -Set the average to the total divided by the counter
+//                                              -Print the average
+//                                           -else
+//                                              -Print “No grades were entered”
+
+// COMPLETE REFINEMENT
+
+//            Initialize total to zero
+//            Initialize counter to zero
+//
+//            Prompt the user to enter the first grade
+//            Input the first grade (possibly the sentinel)
+//
+//            While the user has not yet entered the sentinel
+//                Add this grade into the running total
+//                Add one to the grade counter
+//                Prompt the user to enter the next grade
+//                Input the next grade (possibly the sentinel)
+//
+//            If the counter is not equal to zero
+//                Set the average to the total divided by the counter
+//                Print the average
+//            else
+//                Print “No grades were entered”
+
+
+
+
+
 import java.util.Scanner;
 
 // Solving the class-average problem using sentinel-controlled repetition.
