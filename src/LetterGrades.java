38a804407536dd2905b3e0cef2adc4df277c7e52
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-02-19 23:04:09 +0100

    initialCommit++

--- src/LetterGrades.java
+++ src/LetterGrades.java
@@ -1,8 +1,26 @@
 // LetterGrades class uses the switch statement to count letter grades.
 
+import java.util.Scanner;
+
 public class LetterGrades {
 
     public static void main(String[] args) {
 
+        int total = 0; // sum of grades
+        int gradeCounter = 0; // number of grades entered
+        int aCount = 0; // count of A grades
+        int bCount = 0; // count of B grades
+        int cCount = 0; // count of C grades
+        int dCount = 0; // count of D grades
+        int eCount = 0; // count of E grades
+        int fCount = 0; // count of F grades
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("%s%n%s%n %s%n %s%n",
+                "Enter the integer grades in the range 0-100.",
+                "Type the end-of-file indicator to terminate input:",
+                "On UNIX/Linux/Mac OS X type <Ctrl> d then press Enter",
+                "On Windows type <Ctrl> z then press Enter");
     }
 }
