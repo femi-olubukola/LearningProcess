12b4e2bd11c11bf9bd99062872262d2f566a60cf
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-03-02 11:58:19 +0100

    Scope, MO, Time1

---
+++ src/Time1Test.java
@@ -0,0 +1,36 @@
+// Time1 object used in an app.
+
+public class Time1Test {
+    public static void main(String[] args) {
+
+        // create and initialize a Time1 object
+        Time1 time = new Time1(); // invokes Time1 default constructor
+
+        // output string representations of the time
+        displayTime("After time object is created", time);
+        System.out.println();
+
+        // change time and output updated time
+        time.setTime(13, 27, 6);
+        displayTime("After calling setTime", time);
+        System.out.println();
+
+        // attempt to set time with invalid values
+        try {
+            time.setTime(99,99,99); // all values out of range
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("Exception is " + e.getMessage());
+        }
+
+        // display time after attempt to set invalid values
+        displayTime("After calling setTime with invalid values", time);
+    }
+
+    private static void displayTime(String header, Time1 time) {
+
+        // displays a Time1 object in 24-hour and 12-hour formats
+        System.out.printf("%s%nUniversal time: %s%nStandard time: %s%n",
+                header, time.toUniversalString() , time.toString());
+    }
+}
