b4fa4fb56981805d8b9c5858663d499f78c87fc0
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-03-03 21:20:28 +0100

    packageData && staticImport

---
+++ src/enumTypes/Book.java
@@ -0,0 +1,18 @@
+//package enumTypes;
+//// Declaring an enum type with a constructor and explicit instance fields
+//// and accessors for these fields
+//
+//public enum Book {
+//
+//    // declare constants of enum type
+//    JHTP("Java How to Program", "2015"),
+//    CHTP("C How to Program", "2013"),
+//    IW3HTP("Internet & World Wide Web How to Program", "2012"),
+//    CPPHTP("C++ How to Program", "2014"),
+//    VBHTP("Visual Basic How to Program", "2014"),
+//    CSHARPHTP("Visual C# How to Program", "2014");
+//
+//    // instance fields
+//    private final String title;
+//    private final String copyrightYear
+//}
