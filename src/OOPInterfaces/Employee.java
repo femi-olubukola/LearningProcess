de0d71d3a3327ec3ea6eba720c6f552d8f254d07
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-03-13 21:09:33 +0100

    Employee

--- src/OOPInterfaces/Employee.java
+++ src/OOPInterfaces/Employee.java
@@ -1,5 +1,44 @@
 package OOPInterfaces;
+// Employee abstract superclass that implements Payable.
 
-public class Employee {
+public abstract class Employee implements Payable  {
+
+    private final String firstName;
+    private final String lastName;
+    private final String socialSecurityNumber;
+
+    // constructor
+    public Employee(String firstName, String lastName, String socialSecurityNumber) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    // return first name
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // return last name
+    public String getLastName() {
+        return lastName;
+    }
+
+    // return ssn
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    // return String representation of Employee object
+    @Override
+    public String toString() {
+
+        return String.format("%s %s%nsocial security number: %s",
+            getFirstName(), getLastName(), getSocialSecurityNumber());
+    }
+
+// NOTE: We do not implement Payable method getPaymentAmount here so
+// this class must be declared abstract to avoid a compilation error.
 
 }
