52a8f4282fde955cd98c92b145f767f728946444
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-03-18 21:17:45 +0100

    UsingExceptions++

--- src/ExceptionHandling/UsingExceptions.java
+++ src/ExceptionHandling/UsingExceptions.java
@@ -5,5 +5,15 @@ public class UsingExceptions {
 
     public static void main(String[] args) {
 
+        try
+        {
+            throwException();
+            }
+        catch (Exception exception) // exception thrown by throwException
+        {
+            System.err.println("Exception handled in main");
+            }
+
+        doesNotThrowException();
     }
 }
