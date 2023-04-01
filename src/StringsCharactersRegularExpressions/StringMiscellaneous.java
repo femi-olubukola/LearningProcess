3ecf27f4db28cacf126b393429f6bd715964fbea
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-01 17:50:58 +0100

    StringMiscellaneous++

--- src/StringsCharactersRegularExpressions/StringMiscellaneous.java
+++ src/StringsCharactersRegularExpressions/StringMiscellaneous.java
@@ -16,5 +16,17 @@ public class StringMiscellaneous {
 
         // loop through characters in s1 with charAt and display reversed
         System.out.printf("%nThe string reversed is: ");
-    }
+
+        for (int count = s1.length() - 1; count >= 0; count--)
+            System.out.printf("%c ", s1.charAt(count));
+
+        // copy characters from string into charArray
+        s1.getChars(0, 5, charArray, 0);
+        System.out.printf("%nThe character array is: ");
+
+        for (char character : charArray)
+            System.out.print(character);
+
+        System.out.println();
+}
 }
