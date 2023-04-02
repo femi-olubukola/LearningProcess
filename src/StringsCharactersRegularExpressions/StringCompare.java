92aeb4e07f7aa6e7e45e9741432e7f87cd7cc820
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-02 20:44:10 +0100

    StringCompare

---
+++ src/StringsCharactersRegularExpressions/StringCompare.java
@@ -0,0 +1,15 @@
+package StringsCharactersRegularExpressions;
+
+public class StringCompare {
+
+    public static void main(String[] args) {
+
+        String s1 = new String("hello"); // s1 is a copy of "hello"
+        String s2 = "goodbye";
+        String s3 = "Happy Birthday";
+        String s4 = "happy birthday";
+
+        System.out.printf(
+                "s1 = %s%ns2 = %s%ns3 = %s%ns4 = %s%n%n", s1, s2, s3, s4);
+    }
+}
