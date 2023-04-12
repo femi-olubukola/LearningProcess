51dbde9098eed5097b12b13784cc44222eb03d33
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-12 20:47:48 +0100

    StringBuilderCapLen++

--- src/StringsCharactersRegularExpressions/StringBuilderCapLen.java
+++ src/StringsCharactersRegularExpressions/StringBuilderCapLen.java
@@ -1,9 +1,21 @@
 package StringsCharactersRegularExpressions;
 
+// StringBuilder length, setLength, capacity and ensureCapacity methods.
 public class StringBuilderCapLen {
 
     public static void main(String[] args) {
 
+        StringBuilder buffer = new StringBuilder("Hello, how are you?");
+
+        System.out.printf("buffer = %s%nlength = %d%ncapacity = %d%n%n",
+                buffer.toString(), buffer.length(), buffer.capacity());
+
+        buffer.ensureCapacity(75);
+        System.out.printf("New capacity = %d%n%n", buffer.capacity());
+
+        buffer.setLength(10);
+        System.out.printf("New length = %d%nbuffer = %s%n",
+                buffer.length(), buffer.toString());
 
     }
 }
