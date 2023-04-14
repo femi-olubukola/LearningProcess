0a1ad828506926a04e85b6e8316079a7fed04f8d
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-14 15:24:01 +0100

    StringBuilderAppend++

--- src/StringsCharactersRegularExpressions/StringBuilderAppend.java
+++ src/StringsCharactersRegularExpressions/StringBuilderAppend.java
@@ -14,5 +14,32 @@ public class StringBuilderAppend {
         long longValue = 10000000000L;
         float floatValue = 2.5f;
         double doubleValue = 33.333;
+
+        StringBuilder lastBuffer = new StringBuilder("last buffer");
+        StringBuilder buffer = new StringBuilder();
+
+        buffer.append(objectRef)
+                .append("%n")
+                .append(string)
+                .append("%n")
+                .append(charArray)
+                .append("%n")
+                .append(charArray, 0, 3)
+                .append("%n")
+                .append(booleanValue)
+                .append("%n")
+                .append(characterValue)
+                .append("%n")
+                .append(integerValue)
+                .append("%n")
+                .append(longValue)
+                .append("%n")
+                .append(floatValue)
+                .append("%n")
+                .append(doubleValue)
+                .append("%n")
+                .append(lastBuffer);
+
+        System.out.printf("buffer contains%n%s%n", buffer.toString());
     }
 }
