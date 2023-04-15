47e203548534a427c81e83957b2b539a28935215
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-15 16:36:06 +0100

    StringBuilderInsertDelete

---
+++ src/StringsCharactersRegularExpressions/StringBuilderInsertDelete.java
@@ -0,0 +1,19 @@
+package StringsCharactersRegularExpressions;
+
+public class StringBuilderInsertDelete {
+
+    public static void main(String[] args) {
+
+        Object objectRef = "hello";
+        String string = "goodbye";
+        char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f'};
+        boolean booleanValue = true;
+        char characterValue = 'K';
+        int integerValue = 7;
+        long longValue = 10000000;
+        float floatValue = 2.5f; // f suffix indicates that 2.5 is a float
+        double doubleValue = 33.333;
+
+        StringBuilder buffer = new StringBuilder();
+    }
+}
