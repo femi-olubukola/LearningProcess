70f4eb06b633a10824087351e2c12072ace71cd4
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-17 22:21:22 +0100

    StaticCharMethods++

--- src/StringsCharactersRegularExpressions/StaticCharMethods.java
+++ src/StringsCharactersRegularExpressions/StaticCharMethods.java
@@ -17,6 +17,19 @@ public class StaticCharMethods {
         System.out.printf("is digit: %b%n", Character.isDigit(c));
         System.out.printf("is first character in a Java identifier: %b%n",
                 Character.isJavaIdentifierStart(c));
+        System.out.printf("is part of a Java identifier: %b%n",
+                Character.isJavaIdentifierPart(c));
+        System.out.printf("is letter: %b%n", Character.isLetter(c));
+        System.out.printf(
+                "is letter or digit: %b%n", Character.isLetterOrDigit(c));
+        System.out.printf(
+                "is lower case: %b%n", Character.isLowerCase(c));
+        System.out.printf(
+                "is upper case: %b%n", Character.isUpperCase(c));
+        System.out.printf(
+                "to upper case: %s%n", Character.toUpperCase(c));
+        System.out.printf(
+                "to lower case: %s%n", Character.toLowerCase(c));
 
     }
 }
