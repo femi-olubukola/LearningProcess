9057088060d2d68f86703141084e9473b1e025ca
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-18 14:45:57 +0100

    StaticCharMethods2++

--- src/StringsCharactersRegularExpressions/StaticCharMethods2.java
+++ src/StringsCharactersRegularExpressions/StaticCharMethods2.java
@@ -25,14 +25,14 @@ public class StaticCharMethods2 {
                 System.out.println("Enter a digit:");
                 int digit = scanner.nextInt();
                 System.out.printf("Convert digit to character: %s%n",
-                        );
+                        Character.forDigit(digit, radix));
                 break;
 
             case 2: // convert character to digit
                 System.out.println("Enter a character:");
                 char character = scanner.next().charAt(0);
                 System.out.printf("Convert character to digit: %s%n",
-                    );
+                        Character.digit(character, radix));
                 break;
         }
     }
