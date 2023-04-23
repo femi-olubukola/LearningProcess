db9a1c47e72ba7c4713af31892c8b7788932990a
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-23 19:03:16 +0100

    RegexSubstitution++

--- src/StringsCharactersRegularExpressions/RegexSubstitution.java
+++ src/StringsCharactersRegularExpressions/RegexSubstitution.java
@@ -17,5 +17,25 @@ public class RegexSubstitution {
 
         // replace 'stars' with 'carets'
         firstString = firstString.replaceAll("stars", "carets");
+
+        System.out.printf(
+                "\"carets\" substituted for \"stars\": %s%n", firstString);
+
+        // replace words with 'word'
+        System.out.printf("Every word replaced by \"word\": %s%n%n",
+                firstString.replaceAll("\\w+", "word"));
+
+        System.out.printf("Original String 2: %s%n", secondString);
+
+        // replace first three digits with 'digit'
+        for (int i = 0; i < 3; i++)
+            secondString = secondString.replaceFirst("\\d", "digit");
+
+        System.out.printf(
+                "First 3 digits replaced by \"digit\" : %s%n", secondString);
+
+        System.out.print("String split at commas: ");
+        String[] results = secondString.split(",\\s*"); // split on commas
+        System.out.println( );
     }
 }
