1b01a7cc65c2380a804c6b5b4581fe58080e1217
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-04-30 17:55:44 +0100

    DeckOfCardsTest

--- src/ArrayandArrayLists/CardShuffling/DeckOfCardsTest.java
+++ src/ArrayandArrayLists/CardShuffling/DeckOfCardsTest.java
@@ -11,7 +11,7 @@ public class DeckOfCardsTest {
         // print all 52 Cards in the order in which they are dealt
         for (int i = 1; i <= 52; i++) {
             // deal and display a Card
-            System.out.printf("%-19s", myDeckOfCards);
+            System.out.printf("%-19s", myDeckOfCards.dealCard());
 
             if (i % 4 == 0) // output a newline after every fourth card
                 System.out.println();
