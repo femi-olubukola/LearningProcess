6dcf2e7a7a37be0890a72cee3c7ca360bb97490e
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-05-07 18:37:40 +0100

    DrawRainbow++

--- src/ArrayandArrayLists/DrawRainbow.java
+++ src/ArrayandArrayLists/DrawRainbow.java
@@ -1,9 +1,11 @@
 package ArrayandArrayLists;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
 
 // Drawing a rainbow using arcs and an array of colors.
-public class DrawRainbow {
+public class DrawRainbow extends JPanel {
 
     // define indigo and violet
     private final static Color VIOLET = new Color(128, 0, 128);
@@ -20,5 +22,25 @@ public class DrawRainbow {
         setBackground(Color.WHITE); // set the background to white
         }
 
+    // draws a rainbow using concentric arcs
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
+        int radius = 20; // radius of an arc
+
+        // draw the rainbow near the bottom-center
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() - 10;
+
+        // draws filled arcs starting with the outermost
+        for (int counter = colors.length; counter > 0; counter--) {
+            // set the color for the current arc
+            g.setColor(colors[counter - 1]);
+
+            // fill the arc from 0 to 180 degrees
+            g.fillArc(centerX - counter * radius,
+                    centerY - counter * radius,
+                    counter * radius * 2, counter * radius * 2, 0, 180);
+            }
+    }
 }
