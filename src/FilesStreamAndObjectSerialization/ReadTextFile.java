517782b876948b886216a610f4ef9ee75ff8e8d2
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-05-20 17:59:39 +0100

    readTextFile++

--- src/FilesStreamAndObjectSerialization/ReadTextFile.java
+++ src/FilesStreamAndObjectSerialization/ReadTextFile.java
@@ -1,5 +1,11 @@
 package FilesStreamAndObjectSerialization;
 
+import java.io.IOException;
+import java.lang.IllegalStateException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ReadTextFile {
@@ -12,4 +18,43 @@ public class ReadTextFile {
         readRecords();
         closeFile();
     }
+
+    // open file clients.txt
+    public static void openFile() {
+
+        try
+        {
+            input = new Scanner(Paths.get("clients.txt"));
+        }
+        catch (IOException ioException)
+        {
+            System.err.println("Error opening file. Terminating.");
+            System.exit(1);
+        }
+    }
+
+    // read record from file
+    public static void readRecords() {
+        System.out.printf("%-10s%-12s%-12s%10s%n", "Account",
+                "First Name", "Last Name", "Balance");
+
+        try
+        {
+            while (input.hasNext()) // while there is more to read
+                {
+                // display record contents
+                System.out.printf("%-10d%-12s%-12s%10.2f%n", input.nextInt(),
+                        input.next(), input.next(), input.nextDouble());
+                }
+        }
+        catch (NoSuchElementException elementException)
+        {
+            System.err.println("File improperly formed. Terminating.");
+        }
+        catch (IllegalStateException stateException)
+        {
+            System.err.println("Error reading from file. Terminating.");
+        }
+    } // end method readRecords
+
 }
