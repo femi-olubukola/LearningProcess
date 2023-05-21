454bade3507fb36dd256f9939d6add1a76f231ca
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-05-21 18:10:05 +0100

    a credit inquiry program// menuOption

---
+++ src/FilesStreamAndObjectSerialization/a_credit_inquiry_program/MenuOption.java
@@ -0,0 +1,16 @@
+package FilesStreamAndObjectSerialization.a_credit_inquiry_program;
+
+public enum MenuOption {
+
+    // declare contents of enum type
+    ZERO_BALANCE(1),
+    CREDIT_BALANCE(2),
+    DEBIT_BALANCE(3),
+    END(4);
+
+    private final int value; // current menu option
+
+    private MenuOption(int value) {
+        this.value = value;
+    }
+}
