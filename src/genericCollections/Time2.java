18b44565dca6d9fc5f5b6cd97c3bddecd88847c7
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-05-31 18:58:41 +0100

    time2++

--- src/genericCollections/Time2.java
+++ src/genericCollections/Time2.java
@@ -40,4 +40,27 @@ public class Time2 {
         this.second = second;
     }
 
+    // Time2 constructor: another Time2 object supplied
+    public Time2(Time2 time) {
+        // invoke constructor with three arguments
+        this(time.getHour(), time.getMinute(), time.getSecond());
+    }
+    // Set Methods
+    // set a new time value using universal time;
+    // validate the data
+    public void setTime(int hour, int minute, int second) {
+        if (hour < 0 || hour >= 24)
+            throw new IllegalArgumentException("hour must be 0-23");
+
+        if (minute < 0 || minute >= 60)
+            throw new IllegalArgumentException("minute must be 0-59");
+
+        if (second < 0 || second >= 60)
+            throw new IllegalArgumentException("second must be 0-59");
+
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
 }
