4a553df85d901f20f4cf0d9b4bc7162d2ee8c921
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-06-01 10:01:44 +0100

    sort3++

--- src/genericCollections/Sort3.java
+++ src/genericCollections/Sort3.java
@@ -11,6 +11,11 @@ public class Sort3 {
 
         List<Time2> list = new ArrayList<>(); // create List
 
+        list.add(new Time2(6, 24, 34));
+        list.add(new Time2(18, 14, 58));
+        list.add(new Time2(6, 05, 34));
+        list.add(new Time2(12, 14, 58));
+        list.add(new Time2(6, 24, 22));
 
         // output List elements
         System.out.printf("Unsorted array elements:%n%s%n", list);
