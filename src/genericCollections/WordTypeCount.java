21f3503a9a7aa682e84c6d915e03df6df88ebf2f
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-06-07 19:37:20 +0100

    wordTypeCount

--- src/genericCollections/WordTypeCount.java
+++ src/genericCollections/WordTypeCount.java
@@ -8,4 +8,15 @@ import java.util.TreeSet;
 import java.util.Scanner;
 
 public class WordTypeCount {
+
+    public static void main(String[] args) {
+
+        // create HashMap to store String keys and Integer values
+        Map<String, Integer> myMap = new HashMap<>();
+
+        createMap(myMap); // create map based on user input
+        displayMap(myMap); // display map content
+    }
+
+
 }
