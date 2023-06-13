fc6ecaf078fa902d650ee1bd3836ef802918bdc6
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-06-13 17:39:50 +0100

    arraysAndStreams2++

--- src/lambdasAndStreams/ArraysAndStreams2.java
+++ src/lambdasAndStreams/ArraysAndStreams2.java
@@ -23,5 +23,18 @@ public class ArraysAndStreams2 {
                         .map(String::toUpperCase)
                         .collect(Collectors.toList()));
 
+        // strings less than "n" (case insensitive) sorted ascending
+        System.out.printf("strings greater than m sorted ascending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n") < 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER)
+                        .collect(Collectors.toList()));
+
+        // strings less than "n" (case insensitive) sorted descending
+        System.out.printf("strings greater than m sorted descending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n") < 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                        .collect(Collectors.toList()));
     }
 }
