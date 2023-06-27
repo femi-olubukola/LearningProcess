6ebe722cc20dc9e87c5672ac83f6e4a78a517c57
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-06-27 11:40:38 +0100

    fibonacciCalculator+

--- src/recursion/FibonacciCalculator.java
+++ src/recursion/FibonacciCalculator.java
@@ -1,14 +1,16 @@
 package recursion;
 
+// Recursive fibonacci method.
 
 import java.math.BigInteger;
+
 public class FibonacciCalculator {
 
     private static BigInteger TWO = BigInteger.valueOf(2);
 
     // recursive declaration of method fibonacci
-    public static BigInteger fibonacci(BigInteger number)
-    {
+    public static BigInteger fibonacci(BigInteger number) {
+
         if (number.equals(BigInteger.ZERO) ||
                 number.equals(BigInteger.ONE)) // base cases
             return number;
@@ -16,4 +18,5 @@ public class FibonacciCalculator {
             return fibonacci(number.subtract(BigInteger.ONE)).add(
                     fibonacci(number.subtract(TWO)));
     }
+
 }
