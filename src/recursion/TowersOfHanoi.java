e924ecf1fda2aad00a57626f8856658b5087c3b8
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-06-29 10:30:18 +0100

    towersOfHanoi

---
+++ src/recursion/TowersOfHanoi.java
@@ -0,0 +1,24 @@
+package recursion;
+
+public class TowersOfHanoi {
+
+    // recursively move disks between towers
+    public static void solveTowers(int disks, int sourcePeg,
+                                   int destinationPeg, int tempPeg) {
+
+        // base case -- only one disk to move
+        if (disks == 1) {
+            System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+            return;
+        }
+
+        // recursion step -- move (disk - 1) disks from sourcePeg to tempPeg using destinationPeg
+        solveTowers(disks - 1, sourcePeg, tempPeg, destinationPeg);
+
+        // move last disk from sourcePeg to destinationPeg
+        System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+
+        // move (disks - 1) disks from tempPeg to destinationPeg
+        solveTowers(disks - 1, tempPeg, destinationPeg, sourcePeg);
+    }
+}
