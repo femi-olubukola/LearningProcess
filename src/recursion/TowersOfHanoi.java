d7cf7d23a3f9b2b16a27e0510d95363fc9998238
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-06-30 07:56:29 +0100

    towersOfHanoi++

--- src/recursion/TowersOfHanoi.java
+++ src/recursion/TowersOfHanoi.java
@@ -23,4 +23,15 @@ public class TowersOfHanoi {
         // move (disks - 1) disks from tempPeg to destinationPeg
         solveTowers(disks - 1, tempPeg, destinationPeg, sourcePeg);
     }
+
+    public static void main(String[] args) {
+
+        int startPeg = 1; // value 1 used to indicate startPeg in output
+        int endPeg = 3; // value 3 used to indicate endPeg in output
+        int tempPeg = 2; // value 2 used to indicate tempPeg in output
+        int totalDisks = 3; // number of disks
+
+        // initial nonrecursive call: move all disks.
+        solveTowers(totalDisks, startPeg, endPeg, tempPeg);
+    }
 }
