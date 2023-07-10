595e6cfef11a5986826e8253b7a1a3b8e561572d
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-07-10 08:17:30 +0100

    binarySearchTest

---
+++ src/searchingSortingAndBigO/BinarySearchTest.java
@@ -0,0 +1,43 @@
+package searchingSortingAndBigO;
+
+// Use binary search to locate an item in an array.
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+public class BinarySearchTest {
+
+    // perform a binary search on the data
+    public static int binarySearch(int[] data, int key) {
+
+        int low = 0; // low end of the search area
+        int high = data.length - 1; // high end of the search area
+        int middle = (low + high + 1) / 2; // middle element
+        int location = -1; // return value; -1 if not found
+
+        do // loop to search for element
+        {
+            // print remaining elements of array
+            System.out.print(remainingElements(data, low, high));
+
+            // output spaces for alignment
+            for (int i = 0; i < middle; i++)
+                System.out.print(" ");
+            System.out.println(" * "); // indicate current middle
+
+            // if the element is found at the middle
+            if (key == data[middle])
+                location = middle; // location is the current middle
+            else if (key < data[middle]) // middle element is too high
+                high = middle - 1; // eliminate the higher half
+            else // middle element is too low
+                low = middle + 1; // eliminate the lower half
+            middle = (low + high + 1) / 2; // recalculate the middle
+        } while ((low <= high) && (location == -1));
+
+        return location; // return location of search key
+
+    }  // end method binarySearch
+
+
+}
