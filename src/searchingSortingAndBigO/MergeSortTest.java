dd926e4fbf3631bf40c55955b7c62c26ecdbee8c
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-07-24 08:20:16 +0100

    mergeSortTest+

--- src/searchingSortingAndBigO/MergeSortTest.java
+++ src/searchingSortingAndBigO/MergeSortTest.java
@@ -34,4 +34,46 @@ public class MergeSortTest {
             merge (data, low, middle1, middle2, high);
             } // end if
     } // end method sortArray
-}
+
+    // merge two sorted subarrays into one sorted subarray
+    private static void merge(int[] data, int left, int middle1, int middle2, int right) {
+        int leftIndex = left; // index into left subarray
+        int rightIndex = middle2; // index into right subarray
+        int combinedIndex = left; // index into temporary working array
+        int[] combined = new int[data.length]; // working array
+
+        // output two subarrays before merging
+        System.out.printf("merge: %s%n",
+                subarrayString(data, left, middle1));
+        System.out.printf(" %s%n",
+                subarrayString(data, middle2, right));
+
+        // merge arrays until reaching end of either
+        while (leftIndex <= middle1 && rightIndex <= right) {
+            // place smaller of two current elements into result and move to next space in arrays
+            if (data[leftIndex] <= data[rightIndex])
+                combined[combinedIndex++] = data[leftIndex++];
+            else
+            combined[combinedIndex++] = data[rightIndex++];
+        }
+
+        // if left array is empty
+        if (leftIndex == middle2)
+            // copy in rest of right array
+        while (rightIndex <= right)
+            combined[combinedIndex++] = data[rightIndex++];
+        else // right array is empty
+        // copy in rest of left array
+        while (leftIndex <= middle1)
+            combined[combinedIndex++] = data[leftIndex++];
+
+        // copy values back into original array
+        for (int i = left; i <= right; i++)
+            data[i] = combined[i];
+
+        // output merged array
+        System.out.printf(" %s%n%n",
+                subarrayString(data, left, right));
+    } // end method merge
+
+    }
