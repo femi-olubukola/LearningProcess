66e85ec566f13e8b35789219bffb4f961dfede99
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-08-09 19:30:24 +0100

    maximumTest++

--- src/genericClassesAndMethods/MaximumTest.java
+++ src/genericClassesAndMethods/MaximumTest.java
@@ -1,5 +1,8 @@
 package genericClassesAndMethods;
 
+// Generic method maximum returns the largest of three objects.
+
+
 public class MaximumTest {
 
     public static void main(String[] args) {
@@ -11,5 +14,19 @@ public class MaximumTest {
         System.out.printf("Maximum of %s, %s and %s is %s%n", "pear",
                 "apple", "orange", maximum("pear", "apple", "orange"));
 
+    }
+
+    // determines the largest of three Comparable objects
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z)
+    {
+        T max = x; // assume x is initially the largest
+
+        if (y.compareTo(max) > 0)
+            max = y; // y is the largest so far
+
+        if (z.compareTo(max) > 0)
+            max = z; // z is the largest
+
+        return max; // returns the largest object
     }
 }
