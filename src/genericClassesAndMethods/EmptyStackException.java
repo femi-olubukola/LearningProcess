e3b340b61c247d1b51a2a236427aa7caebac7c03
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-08-15 16:50:19 +0100

    emptyStackException

---
+++ src/genericClassesAndMethods/EmptyStackException.java
@@ -0,0 +1,16 @@
+package genericClassesAndMethods;
+
+// EmptyStackException class declaration.
+
+public class EmptyStackException extends RuntimeException {
+
+    // no-argument constructor
+    public EmptyStackException() {
+        this("Stack is empty");
+        }
+
+        // one-argument constructor
+        public EmptyStackException(String message) {
+        super(message);
+    }
+}
