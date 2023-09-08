40414b2366dda460b0cd8ce7a82c069b33e895fe
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-09-08 09:04:02 +0100

    wildcardTest+

--- src/genericClassesAndMethods/WildcardTest.java
+++ src/genericClassesAndMethods/WildcardTest.java
@@ -30,5 +30,18 @@ public class WildcardTest {
 
         System.out.printf("doubleList contains: %s%n", doubleList);
         System.out.printf("Total of the elements in doubleList: %.1f%n%n", sum(doubleList));
+
+
+        // create, initialize and output ArrayList of Numbers containing
+        // both Integers and Doubles, then display total of the elements
+        Number[] numbers = {1, 2.4, 3, 4.1}; // Integers and Doubles
+        ArrayList<Number> numberList = new ArrayList<>();
+
+        // insert elements in numberList
+        for (Number element : numbers)
+            numberList.add(element);
+
+        System.out.printf("numberList contains: %s%n", numberList);
+        System.out.printf("Total of the elements in numberList: %.1f%n", sum(numberList));
     }
 }
