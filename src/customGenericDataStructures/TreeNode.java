07cd480bb5887039b22503ecc3caa5d4e3bd44e4
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-10-30 21:02:38 +0100

    treeNode+

--- src/customGenericDataStructures/TreeNode.java
+++ src/customGenericDataStructures/TreeNode.java
@@ -15,4 +15,24 @@ public class TreeNode<T extends Comparable<T>> {
         data = nodeData;
         leftNode = rightNode = null; // node has no children
     }
-}
+
+    // locate insertion point and insert new node; ignore duplicate values
+    public void insert(T insertValue) {
+        // insert in left subtree
+        if (insertValue.compareTo(data) < 0) {
+            // insert new TreeNode
+            if (leftNode == null)
+                leftNode = new TreeNode<T>(insertValue);
+            else // continue traversing left subtree recursively
+            leftNode.insert(insertValue);
+        }
+        // insert in right subtree
+        else if (insertValue.compareTo(data) > 0) {
+            // insert new TreeNode
+            if (rightNode == null)
+                rightNode = new TreeNode<T>(insertValue);
+            else // continue traversing right subtree recursively
+            rightNode.insert(insertValue);
+            }
+        }
+} // end class TreeNode
