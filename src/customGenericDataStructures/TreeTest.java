4a6727aade82419c54ea0381261efbff59ce8ce4
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-11-04 03:36:32 +0100

    treeTest

---
+++ src/customGenericDataStructures/TreeTest.java
@@ -0,0 +1,15 @@
+package customGenericDataStructures;
+
+import java.security.SecureRandom;
+
+// Binary tree test program.
+public class TreeTest {
+
+    public static void main(String[] args) {
+
+        Tree<Integer> tree = new Tree<Integer>();
+        SecureRandom randomNumber = new SecureRandom();
+
+        System.out.println("Inserting the following values: ");
+    }
+}
