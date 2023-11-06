8e3477016bbfc25439fd84df3d37e0966326d022
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-11-06 14:03:40 +0100

    treeTest+

--- src/customGenericDataStructures/TreeTest.java
+++ src/customGenericDataStructures/TreeTest.java
@@ -11,5 +11,13 @@ public class TreeTest {
         SecureRandom randomNumber = new SecureRandom();
 
         System.out.println("Inserting the following values: ");
+
+        // insert 10 random integers from 0-99 in tree
+        for (int i = 1; i <= 10; i++) {
+            int value = randomNumber.nextInt(100);
+            System.out.printf("%d ", value);
+            tree.insertNode(value);
+
+        }
     }
 }
